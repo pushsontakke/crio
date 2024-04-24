2c87da2d11691598b4f00ea4a950ceff451508ef
pushsontakke <pusontakke1@gmail.com>
2024-04-24 19:17:17 +0530

    fix Navigation position problem

--- medify/src/components/Hero-Section/HeroSection.jsx
+++ medify/src/components/Hero-Section/HeroSection.jsx
@@ -1,16 +1,19 @@
 import Navigation from "../Navigation/Navigation";
-
+import DoctorsImg from "../../assets/Doctors.png";
+import "./HeroSection.css";
 
 const Hero = () => {
-    return (
-        <div className="Hero">
-            <Navigation />        
-            <div className="container">
-                <div className="left-section"></div>
-                <div className="right-section"><img src="../../assets/Doctors.png" alt="DoctorsImage" /></div>
-            </div>
+  return (
+    <div className="Hero">
+      <Navigation />
+      <div className="container">
+        <div className="left-section"></div>
+        <div className="right-section">
+          <img className="DoctorsImg" src={DoctorsImg} alt="DoctorsImage" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
